aca5016f6d6336acf9a1c0883026817d02f1b2cc
Abhi_360 <abhijeetsakhre.sitele@gmail.com>
2023-03-01 22:55:03 +0530

    Program which find Second smallest element in given array with sorting

---
+++ src/SecondSmallestElementArray.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class SecondSmallestElementArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size=sc.nextInt();
+        int arr[]=new int[size];
+        for(int i=0;i<size;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        int temp;
+        for(int i=0;i<size;i++)
+        {
+            for(int j=i+1;j<size;j++)
+            {
+                if(arr[i]>arr[j])
+                {
+                    temp=arr[i];
+                    arr[i]=arr[j];
+                    arr[j]=temp;
+                }
+            }
+        }
+        System.out.println("Second Smallest element : "+ arr[1]);
+    }
+}
